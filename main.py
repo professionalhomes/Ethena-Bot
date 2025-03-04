ea3eed5d722f2738772b1fbef245d396677fb59c
professionalhomes <lednerdonny5@gmail.com>
2025-03-04 18:19:47 +0800

    upload template

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+bot = discord.Bot(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+if __name__ == '__main__':
+    extensions = [
+        'cogs.ping',
+
+    ]
+    for extension in extensions:
+        bot.load_extension(extension)
+
+load_dotenv()
+bot.run(os.getenv('DISCORD_BOT_TOKEN'))
